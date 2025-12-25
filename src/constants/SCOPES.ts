db3f7b348b6ec43757b18c923bb0eacca9f5c180
Gerardo-Ontiveros <gontiverosdavila01@gamail.com>
2025-12-25 14:26:13 -0600

    Update SCOPES to include additional channel management permissions

--- src/constants/SCOPES.ts
+++ src/constants/SCOPES.ts
@@ -1,6 +1,17 @@
 export const SCOPES = [
+  // Chat básico
   "chat:read",
   "chat:edit",
+
+  "channel:moderate",
+  "moderator:manage:banned_users",
+  "moderator:manage:chat_messages",
+  "moderator:manage:announcements",
+  "moderator:manage:shoutouts",
+
   "channel:manage:broadcast",
   "channel:manage:predictions",
+  "channel:manage:polls",
+
+  "moderation:read",
 ];
